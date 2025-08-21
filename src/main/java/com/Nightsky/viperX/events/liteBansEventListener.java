f56b1b8b8eaa5e8d75c30b690f9c92466b60985c
NotNightSky <okmon110@gmail.com>
2025-08-21 14:48:51 +0530

    Version 1.2.0 clearinventory now works with perm bans

--- src/main/java/com/Nightsky/viperX/events/liteBansEventListener.java
+++ src/main/java/com/Nightsky/viperX/events/liteBansEventListener.java
@@ -4,7 +4,8 @@ import litebans.api.Entry;
 import litebans.api.Events;
 import org.bukkit.Bukkit;
 
-import java.io.File;
+import org.bukkit.entity.Player;
+
 import java.util.UUID;
 
 public class liteBansEventListener{
@@ -14,16 +15,11 @@ public class liteBansEventListener{
             public void entryAdded(Entry entry) {
                 if ("ban".equals(entry.getType())) {
                     if (entry.isPermanent()) {
-                        UUID id = UUID.fromString(entry.getUuid());
+                        Player player = Bukkit.getPlayer(UUID.fromString(entry.getUuid()));
 
-                        File playerDataFileMain = new File(Bukkit.getWorlds().getFirst().getWorldFolder(), "playerdata" + File.separator + id + ".dat");
-
-                        playerDataFileMain.delete();
-
-                        File playerDataFileOld = new File(Bukkit.getWorlds().getFirst().getWorldFolder(), "playerdata" + File.separator + id + ".dat_old");
-
-                        playerDataFileOld.delete();
-                    } else {
+                        player.getInventory().clear();
+                        player.updateInventory();
+                    }else {
                         System.out.println("[LiteBans] Player temporarily banned: "
                                 + entry.getUuid()
                                 + " Reason: " + entry.getReason()
